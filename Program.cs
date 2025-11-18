85fb3cea4dc9dfd72643d8fd4844ce64aaaccf7c
Lucas Carbo <lucas@example.com>
2025-11-18 09:39:06 -0300

    Fix: Add missing using directives and fix project SDK

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TestingWindowsService
 {
